1772dafa26fc40c8c049a44ca011a3e8a6186ae7
Hüseyin TERKİR <hsynterkr@gmail.com>
2017-12-20 01:43:26 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,6 @@ router.post('/', function (req, res) {
         console.log(req.body.username);
         account = req.body.username;
 
-
-
-
         sbd_api_url = "https://min-api.cryptocompare.com/data/price?fsym=SBD&tsyms=TRY,USD";
         request.get(sbd_api_url, { json: true }, function (err, res, body) {
             console.log(body);
@@ -34,20 +31,13 @@ router.post('/', function (req, res) {
 
         steem.api.getAccounts([account], function(err, result) {
             console.log(err, result);
-            console.log('Name: '+ result[0].name );
             name = result[0].name;
-            console.log('STEEM Balance: '+ result[0].balance );
             steem_balance = result[0].balance;
-            console.log('Reputation: '+ ( result[0].reputation / reput ).toFixed(2) );
             rep = (result[0].reputation / reput).toFixed(2);
-            console.log('SBD Balance: '+ result[0].sbd_balance );
             sbd_balance = result[0].sbd_balance;
-
             sbd_usd = (parseFloat(sbd_balance)) * sbd_api_usd;
             steem_usd = (parseFloat(steem_balance)) * steem_api_usd;
             sum_usd = sbd_usd + steem_usd;
-
-
             sbd_try = (parseFloat(sbd_balance)) * (parseFloat(sbd_try));
             steem_try = (parseFloat(steem_balance)) * (parseFloat(steem_try));
             sum_try = sbd_try + steem_try;
@@ -64,14 +54,7 @@ router.post('/', function (req, res) {
             sum_usd:(sum_usd).toFixed(2),
             sum_try:(sum_try).toFixed(2)
         });
-
-    }, 9000);
-
-
-
-
-
-
+    }, 15000);
     }
 );
 
@@ -88,7 +71,5 @@ router.get('/', function(req, res, next) {
         sum_try:sum_try
     });
 });
-
-
 
 module.exports = router;
